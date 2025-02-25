0d378ce114434f757a1bae693e2d328ebda47f6d
nyamu <i@nyamuchi.com>
2025-02-25 17:06:01 +0800

    优化网站基础数据管理，统一模板数据获取方式并完善页面元素

--- main.go
+++ main.go
@@ -11,6 +11,30 @@ import (
 
 func main() {
 	engine := html.New("web/templates", ".html")
+	engine.AddFunc("GetBaseData", func(key string) any {
+		data := map[string]any{
+			"title":    "Nyamuchi_",
+			"username": "nyamuchi",
+			"email":    "i@nyamuchi.com",
+			"img":      "https://nyamuchi.com/_astro/avatar.hlvXZsLg_Z22HApl.webp",
+			"motto":    "莫愁前路无知己",
+			"navtab": []fiber.Map{
+				{"name": "首页", "link": "/"},
+				{"name": "标签", "link": "/tags"},
+				{"name": "关于", "link": "/about"},
+				{"name": "友链", "link": "/friends"},
+				{"name": "联系我", "link": "/contactme"},
+			},
+			"aboutsite": "分享Web开发经验与技术见解，记录学习成长之路。",
+			"social": []fiber.Map{
+				{"name": "BILIBILI", "link": "https://bilibili.com", "icon": "fa-brands fa-bilibili"},
+				{"name": "GITHUB", "link": "https://github.com", "icon": "fa-brands fa-github"},
+				{"name": "Twitter/X", "link": "https://x.com", "icon": "fa-brands fa-x"},
+				{"name": "Discord", "link": "https://discord.com", "icon": "fa-brands fa-discord"},
+			},
+		}
+		return data[key]
+	})
 	app := fiber.New(fiber.Config{
 		Views: engine,
 	})
--- router/router.go
+++ router/router.go
@@ -4,8 +4,13 @@ import (
 	"github.com/gofiber/fiber/v3"
 )
 
+var data = fiber.Map{
+	"title": "",
+}
+
 func Setup(app *fiber.App) {
+
 	app.Get("/", func(c fiber.Ctx) error {
-		return c.Render("index", fiber.Map{}, "Layout/main")
+		return c.Render("index", data, "Layout/main")
 	})
 }
