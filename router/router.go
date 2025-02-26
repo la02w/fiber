fa5c6bc49276e49993d70c1b6662e82f992e1dc1
nyamu <i@nyamuchi.com>
2025-02-26 10:00:42 +0800

    添加新的路由页面及完善 Tailwind CSS 样式类和 CSS 变量

--- router/router.go
+++ router/router.go
@@ -13,4 +13,20 @@ func Setup(app *fiber.App) {
 	app.Get("/", func(c fiber.Ctx) error {
 		return c.Render("index", data, "Layout/main")
 	})
+
+	app.Get("/about", func(c fiber.Ctx) error {
+		return c.Render("pages/about", fiber.Map{}, "Layout/main")
+	})
+
+	app.Get("/friends", func(c fiber.Ctx) error {
+		return c.Render("pages/friend", fiber.Map{}, "Layout/main")
+	})
+
+	app.Get("/post/:name", func(c fiber.Ctx) error {
+		return c.Render("pages/post", fiber.Map{}, "Layout/main")
+	})
+
+	app.Get("/tags", func(c fiber.Ctx) error {
+		return c.Render("tag", fiber.Map{}, "Layout/main")
+	})
 }
